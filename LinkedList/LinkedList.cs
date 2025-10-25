0bb10f3806ca5f71f1cda7aa9b6ab1f994f5e8cb
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2025-10-25 17:25:10 +0200

    Added Linked List + tests

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,72 @@
+﻿namespace LinkedList
+{
+    public class Node<T>
+    {
+        public T Data;
+        public Node<T>? Next;
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    public class LinkedList<T>
+    {
+        private Node<T>? head;
+
+        public LinkedList()
+        {
+            head = null;
+        }
+
+        public void Push(T value)
+        {
+            var newNode = new Node<T>(value);
+            newNode.Next = head;
+            head = newNode;
+        }
+
+        public void Remove(T value)
+        {
+            if (head == null)
+                throw new InvalidOperationException("Nodes are empty.");
+
+            if (head.Data!.Equals(value))
+            {
+                head = head.Next;
+                return;
+            }
+
+            var current = head;
+
+            while (current.Next != null)
+            {
+                if (current.Next.Data!.Equals(value))
+                {
+                    current.Next = current.Next.Next;
+                    return;
+                }
+                current = current.Next;
+            }
+        }
+
+        public Node<T>? Head => head;
+
+        public int Count
+        {
+            get
+            {
+                int count = 0;
+                var current = head;
+                while (current != null)
+                {
+                    count++;
+                    current = current.Next;
+                }
+                return count;
+            }
+        }
+    }
+}
---
+++ Tests/LinkedListTests.cs
@@ -0,0 +1,58 @@
+﻿using LinkedList;
+
+namespace Tests
+{
+    public class LinkedListTests
+    {
+        [Test]
+        public void GivenLinkedList_WhenInitialized_ThenCreateNewLinkedListWith0Nodes()
+        {
+            // arrange
+            var list = new LinkedList.LinkedList<int>();
+
+            // act/assert
+            Assert.That(list.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GivenLinkedList_WhenTwoNodesPushed_ThenListContainsTwoNodesInCorrectOrder()
+        {
+            // arrange
+            var list = new LinkedList.LinkedList<string>();
+
+            // act
+            list.Push("A");
+            list.Push("B");
+
+            // assert
+            Assert.That(list.Count, Is.EqualTo(2));
+            Assert.That(list.Head!.Data, Is.EqualTo("B"));
+            Assert.That(list.Head.Next!.Data, Is.EqualTo("A"));
+        }
+
+        [Test]
+        public void GivenLinkedList_WhenRemove_ThenReturnFirstNodeAndRemoveFromLinkedList()
+        {
+            // arrange
+            var list = new LinkedList.LinkedList<string>();
+            list.Push("A");
+            list.Push("B");
+
+            // act
+            list.Remove("B");
+
+            // assert
+            Assert.That(list.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GivenEmptyLinkedList_WhenRemoveAnEmptyLinkedList_ThrowsInvalidOperationalException()
+        {
+            // arrange
+            var list = new LinkedList.LinkedList<string>();
+
+            // act / assert
+            Assert.Throws<InvalidOperationException>(() => list.Remove(""));
+        }
+    }
+}
