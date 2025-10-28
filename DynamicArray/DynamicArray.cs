d47917dfa7d45fcc4814fee9955c17d85f92e6c4
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2025-10-28 18:34:55 +0100

    Added dynamic array + tests

---
+++ DynamicArray/DynamicArray.cs
@@ -0,0 +1,105 @@
+﻿namespace DynamicArray
+{
+    public class DynamicArray
+    {
+        private int size;
+        private int capacity = 10;
+        private Object[] array;
+
+        public int Size => size;
+
+        public DynamicArray()
+        {
+            array = new Object[capacity];
+        }
+
+        public DynamicArray(int capacity)
+        {
+            this.capacity = capacity;
+            array = new Object[capacity];
+        }
+
+        public void Add(Object value)
+        {
+            if (size>= capacity)
+            {
+                Grow();
+            }
+            array[size] = value;
+            size++;
+        }
+
+        public void Insert(int index, Object value)
+        {
+            if(size>= capacity)
+            {
+                Grow();
+            }
+            for (int i = size; i > index; i--)
+            {
+                array[i] = array[i - 1];
+            }
+            array[index] = value;
+            size++;
+        }
+
+        public void Delete(Object value)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (array[i] == value)
+                {
+                    for(int j = 0; j < (size - i - 1); j++)
+                    {
+                        array[i +j] = array[i + j + 1];
+                    }
+                    array[size - 1] = null;
+                    size--;
+                    if (size <=(int) (capacity/3))
+                    {
+                        Shrink();
+                    }
+                    break;
+                }
+            }
+        }
+
+        public int Search(Object value)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if(array[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void Grow()
+        {
+            int newCapacity = (int)(capacity * 2);
+            Object[] newArray = new Object[newCapacity];
+
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = array[i];
+            }
+            capacity = newCapacity;
+            array = newArray;
+        }
+
+        public void Shrink()
+        {
+            int newCapacity = (int)(capacity / 2);
+            Object[] newArray = new Object[newCapacity];
+
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = array[i];
+            }
+            capacity = newCapacity;
+            array = newArray;
+        }
+    }
+}
---
+++ Tests/DynamicArrayTests.cs
@@ -0,0 +1,133 @@
+﻿using DynamicArray;
+
+namespace Tests
+{
+    public class DynamicArrayTests
+    {
+        [Test]
+        public void GivenDynamicArray_WhenInitialized_ThenArrayIsEmpty()
+        {
+            // arrange & act
+            var array = new DynamicArray.DynamicArray();
+
+            // assert
+            Assert.That(array.Search("Zdechlak"), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void GivenDynamicArray_WhenAddCalled_ThenElementIsAppended()
+        {
+            // arrange
+            var array = new DynamicArray.DynamicArray();
+
+            // act
+            array.Add("A");
+            array.Add("B");
+
+            // assert
+            Assert.That(array.Search("A"), Is.EqualTo(0));
+            Assert.That(array.Search("B"), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GivenDynamicArray_WhenInsertAtIndex_ThenElementIsInsertedAtCorrectPosition()
+        {
+            // arrange
+            var array = new DynamicArray.DynamicArray();
+
+            array.Add("A");
+            array.Add("B");
+            array.Add("C");
+
+            // act
+            array.Insert(0, "X");
+
+            // assert
+            Assert.That(array.Search("X"), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GivenDynamicArray_WhenInsertTriggersGrow_ThenCapacityIsIncreased()
+        {
+            // arrange
+            var array = new DynamicArray.DynamicArray(1);
+
+            // act
+            array.Add("A");
+            array.Add("B");
+
+            // assert
+            Assert.That(array.Size, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void GivenDynamicArray_WhenDeleteExistingValue_ThenValueIsRemovedAndArrayIsShifted()
+        {
+            // arrange
+            var array = new DynamicArray.DynamicArray();
+
+            array.Add("A");
+            array.Add("B");
+
+            // act
+            array.Delete("A");
+
+            // assert
+            Assert.That(array.Size, Is.EqualTo(1));
+            Assert.That(array.Search("B"), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GivenDynamicArray_WhenDeleteNonExistingValue_ThenArrayRemainsUnchanged()
+        {
+            // arrange
+            var array = new DynamicArray.DynamicArray();
+            array.Add("A");
+
+            // act
+            array.Delete("Džonik");
+
+            // assert
+            Assert.That(array.Size, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GivenDynamicArray_WhenDeleteTriggersShrink_ThenCapacityIsReduced()
+        {
+            // arrange
+            var array = new DynamicArray.DynamicArray();
+
+            array.Add("A");
+            array.Add("B");
+            array.Add("C");
+
+            // act
+            array.Delete("B");
+            array.Delete("C");
+
+            // assert
+            Assert.That(array.Size, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GivenDynamicArray_WhenSearchExistingValue_ThenReturnCorrectIndex()
+        {
+            // arrange / act
+            var array = new DynamicArray.DynamicArray();
+            array.Add("A");
+
+            // assert
+            Assert.That(array.Search("A"), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GivenDynamicArray_WhenSearchNonExistingValue_ThenReturnMinusOne()
+        {
+            // arrange / act
+            var array = new DynamicArray.DynamicArray();
+
+            // assert
+            Assert.That(array.Search("A"), Is.EqualTo(-1));
+        }
+    }
+}
