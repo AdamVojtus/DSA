523031c97e7e5850acd8884247e10e315e42ad71
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2026-02-02 21:46:05 +0100

    Optimalization

---
+++ Tests/ArraySumariztionTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Tests
+{
+    public class ArraySumariztionTests
+    {
+        [Test]
+        public void GivenArrayAndTargetSum_WhenPairExists_ThenReturnTrue()
+        {
+            // arrange
+            int[] input = { 6, 2, -1, 7, 4 };
+            int targetSum = 6;
+
+            // act
+            bool result = Arrays.ArraySumarization.DoesArrayContainsTwoNumbersSum(input, targetSum);
+
+            // assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void GivenArrayAndTargetSum_WhenPairDoesNotExist_ThenReturnFalse()
+        {
+            // arrange
+            int[] input = { 1, 2, 3, 4, 5 };
+            int targetSum = 10;
+
+            // act
+            bool result = Arrays.ArraySumarization.DoesArrayContainsTwoNumbersSum(input, targetSum);
+
+            // assert
+            Assert.That(result, Is.False);
+        }
+    }
+}
