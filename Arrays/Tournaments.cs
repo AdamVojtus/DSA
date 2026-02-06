1ed33a40ca4a9fec0bc37a656273a531fb043706
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2026-02-06 21:16:39 +0100

    Added Tournaments Algorithm

---
+++ Arrays/Tournaments.cs
@@ -0,0 +1,61 @@
+﻿namespace Arrays
+{
+    public class Tournaments
+    {
+        public string WinnerOfTournament(List<Match> matches, int[] results)
+        {
+            var scores = new Dictionary<string, int>();
+
+            for (int i = 0; i < matches.Count; i++)
+            {
+                var match = matches[i];
+                var result = results[i];
+                string winnerName;
+
+                if (result == 1)
+                {
+                    winnerName = match.HomeTeam;
+                }
+                else
+                {
+                    winnerName = match.IncomingTeam;
+                }
+
+                if (scores.ContainsKey(winnerName))
+                {
+                    scores[winnerName] += 3;
+                }
+                else
+                {
+                    scores.Add(winnerName, 3);
+                }
+            }
+
+            string absoluteWinner = string.Empty;
+            int maxScore = -1;
+
+            foreach (var entry in scores)
+            {
+                if (entry.Value > maxScore)
+                {
+                    maxScore = entry.Value;
+                    absoluteWinner = entry.Key;
+                }
+            }
+
+            return absoluteWinner;
+        }
+    }
+
+    public class Match
+    {
+        public string HomeTeam { get; }
+        public string IncomingTeam { get; }
+
+        public Match(string homeTeam, string incomingTeam)
+        {
+            HomeTeam = homeTeam ?? throw new ArgumentNullException(nameof(homeTeam));
+            IncomingTeam = incomingTeam ?? throw new ArgumentNullException(nameof(incomingTeam));
+        }
+    }
+}
---
+++ Tests/TournamentsTests.cs
@@ -0,0 +1,55 @@
+﻿using Arrays;
+
+namespace Tests
+{
+    public class TournamentsTests
+    {
+        [Test]
+        public void WhenGivenTeam_AndIsAtHome_ThenReturnTrue()
+        {
+            // arrange
+            List<Match> matches = new List<Match>
+            {
+                new Match("TK", "CA"),
+                new Match("MV", "TK"),
+                new Match("CA", "MV")
+            };
+
+            int[] results = { 1, 0, 0 };
+
+            // act
+            var tournaments = new Tournaments();
+            var result = tournaments.WinnerOfTournament(matches, results);
+
+            // assert
+            Assert.That(result, Is.EqualTo("TK"));
+        }
+
+        [Test]
+        public void WhenGivenTournament_ThenReturnCorrectWinner()
+        {
+            List<Match> matches = new List<Match>
+            {
+                new Match("a", "b"),
+                new Match("b", "c"),
+                new Match("c", "a")
+            };
+
+            int[] results1 = { 1, 0, 1 };
+            int[] results2 = { 0, 0, 1 };
+            int[] results3 = { 1, 1, 0 };
+            int[] results4 = { 0, 1, 0 };
+
+            var tournaments = new Tournaments();
+            var res1 = tournaments.WinnerOfTournament(matches, results1);
+            var res2 = tournaments.WinnerOfTournament(matches, results2);
+            var res3 = tournaments.WinnerOfTournament(matches, results3);
+            var res4 = tournaments.WinnerOfTournament(matches, results4);
+
+            Assert.That(res1, Is.EqualTo("c"));
+            Assert.That(res2, Is.EqualTo("c"));
+            Assert.That(res3, Is.EqualTo("a"));
+            Assert.That(res4, Is.EqualTo("b"));
+        }
+    }
+}
