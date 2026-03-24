870c385eb6f6f0c4f74e362a3af24263bc70b14d
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2026-03-24 18:05:53 +0100

    Optimalization

---
+++ CharacterRecognition/CharacterRecognition.cs
@@ -0,0 +1,29 @@
+﻿namespace CharacterRecognition
+{
+    public class CharacterRecognition
+    {
+        public static bool IsCharacterRecognized(string availableCharacters, string possibleText)
+        {
+            int[] counts = new int[256];
+
+            for (int i = 0; i < availableCharacters.Length; i++)
+            {
+                counts[availableCharacters[i]]++;
+            }
+
+            for (int i = 0; i < possibleText.Length; i++)
+            {
+                char current = possibleText[i];
+
+                if (counts[current] == 0)
+                {
+                    return false;
+                }
+
+                counts[current]--;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Tests/CharacterRecognitionTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Tests
+{
+    public class CharacterRecognitionTests
+    {
+        [Test]
+        public void TestIsCharacterRecognized()
+        {
+            // Arrange
+            var availableCharacters = "dcca";
+            var possibleText1 = "acdc";
+            var possibleText2 = "ac dc";
+            var possibleText3 = "acc";
+            var possibleText4 = "acDc";
+            var possibleText5 = "acca";
+
+            // Act & Assert
+            Assert.IsTrue(CharacterRecognition.CharacterRecognition.IsCharacterRecognized(availableCharacters, possibleText1));
+            Assert.IsFalse(CharacterRecognition.CharacterRecognition.IsCharacterRecognized(availableCharacters, possibleText2));
+            Assert.IsTrue(CharacterRecognition.CharacterRecognition.IsCharacterRecognized(availableCharacters, possibleText3));
+            Assert.IsFalse(CharacterRecognition.CharacterRecognition.IsCharacterRecognized(availableCharacters, possibleText4));
+            Assert.IsFalse(CharacterRecognition.CharacterRecognition.IsCharacterRecognized(availableCharacters, possibleText5));
+        }
+    }
+}
