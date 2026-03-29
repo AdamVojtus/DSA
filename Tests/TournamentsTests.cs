a08af3ff10cef1218df438142434c04d5c01027d
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2026-03-29 20:01:57 +0200

    Fixes

--- Tests/TournamentsTests.cs
+++ Tests/TournamentsTests.cs
@@ -24,5 +24,25 @@ namespace Tests
             // assert
             Assert.That(result, Is.EqualTo("TK"));
         }
+
+        [Test]
+        public void GivenArrayAndTargetSum_WhenIndexOutOfAllowed_ThenReturnFalse()
+        {
+            // arrange
+            List<Match> matches = new List<Match>
+            {
+                new Match("TK", "CA"),
+                new Match("MV", "TK"),
+                new Match("CA", "MV")
+            };
+
+            int[] results = { -1, 0, 1 };
+
+            // act & assert
+            foreach (var match in matches)
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(() => match.GetWinner(results[0]));
+            }
+        }
     }
 }
