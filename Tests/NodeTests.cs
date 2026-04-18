34e812362f6ac3c13e17cfea145d9a223c2147c2
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2026-04-18 17:06:05 +0200

    Added Binary Search

---
+++ Tests/NodeTests.cs
@@ -0,0 +1,30 @@
+﻿using Trees;
+
+namespace Tests
+{
+    public class NodeTests
+    {
+        [Test]
+        public void GivenSortedArray_WhenRootExists_ThenReturnTrue()
+        {
+            var inputs = new int[] { 5, 2, 10, 1, 13, 5, 14, 12, 11 };
+            var result = Node.CreateBalancedTree(inputs);
+
+            Assert.That(result!.Value, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void GivenInvalidLeftNode_WhenLeftValueIsGreater_ThenThrowArgumentException()
+        {
+            var leftNode = new Node(15);
+            Assert.Throws<System.ArgumentException>(() => new Node(10, leftNode, null));
+        }
+
+        [Test]
+        public void GivenInvalidRightNode_WhenRightValueIsSmaller_ThenThrowArgumentException()
+        {
+            var rightNode = new Node(5);
+            Assert.Throws<System.ArgumentException>(() => new Node(10, null, rightNode));
+        }
+    }
+}
---
+++ Trees/Node.cs
@@ -0,0 +1,50 @@
+﻿namespace Trees
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node? Left { get; set; }
+        public Node? Right { get; set; }
+
+        public Node(int value, Node? left = null, Node? right = null)
+        {
+            if (left != null && left.Value >= value)
+            {
+                throw new ArgumentException("Left Node has to be less than the current node value.");
+            }
+
+            if (right != null && right.Value < value)
+            {
+                throw new ArgumentException("Right Node has to be more than the current node value.");
+            }
+
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+
+        public static Node? CreateBalancedTree(int[] inputs)
+        {
+            int[] sortedInput = GetSortedValues(inputs);
+
+            for (int i = 0; i < sortedInput.Length; i++)
+            {
+               if (sortedInput[i] > sortedInput.Length)
+               {
+                    return new Node(sortedInput[i], CreateBalancedTree(sortedInput[..i]), CreateBalancedTree(sortedInput[(i + 1)..]));
+               }
+            }
+
+            return null;
+        }
+
+        public static int[] GetSortedValues(int[] input)
+        {
+            int[] sortedValues = (int[])input.Clone();
+
+            Array.Sort(sortedValues);
+
+            return sortedValues;
+        }
+    }
+}
