154d768cb2a2a174f864293a4201b912e5ef6c93
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2026-05-18 20:07:45 +0200

    Added Synonyms

---
+++ Strings/Synonyms.cs
@@ -0,0 +1,45 @@
+﻿namespace Strings
+{
+    public class Synonyms
+    {
+        public string[][] AreSynonyms(List<string[]> synonymGroups)
+        {
+            var parentMap = new Dictionary<string, string>();
+
+            string GetRoot(string word)
+            {
+                if (!parentMap.ContainsKey(word))
+                {
+                    parentMap[word] = word;
+                }
+
+                if (parentMap[word] != word)
+                {
+                    parentMap[word] = GetRoot(parentMap[word]);
+                }
+
+                return parentMap[word];
+            }
+
+            foreach (var group in synonymGroups)
+            {
+                if (group.Length == 0) continue;
+
+                string root = GetRoot(group[0]);
+                for (int i = 1; i < group.Length; i++)
+                {
+                    string rootParent = GetRoot(group[i]);
+                    if (root != rootParent)
+                    {
+                        parentMap[rootParent] = root;
+                    }
+                }
+            }
+
+            return parentMap.Keys
+                .GroupBy(GetRoot)
+                .Select(group => group.ToArray())
+                .ToArray();
+        }
+    }
+}
---
+++ Tests/SynonymsTests.cs
@@ -0,0 +1,78 @@
+﻿namespace Tests
+{
+    public class SynonymsTests
+    {
+        [Test]
+        public void GivenList_WhenSynonymsExists_ThenReturnCorrectMergedGroup()
+        {
+            // arrange
+            var synonymGroups = new List<string[]>
+            {
+                new[] { "car", "automobile" },
+                new[] { "automobile", "vehicle" },
+                new[] { "happy", "joyful" },
+                new[] { "joyful", "glad" },
+                new[] { "sad", "unhappy" }
+            };
+
+            // act
+            var result = new Strings.Synonyms().AreSynonyms(synonymGroups);
+
+            // assert
+            var expected = new string[][]
+            {
+                ["car", "automobile", "vehicle"],
+                ["happy", "joyful", "glad"],
+                ["sad", "unhappy"]
+            };
+
+            Assert.That(result, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void GivenList_WhenMoreSynonymsExists_ThenReturnCorrectMergedGroup()
+        {
+            // arrange
+            var synonymGroups = new List<string[]>
+            {
+                new[] { "car", "automobile", "vehicle" },
+                new[] { "automobile", "vehicle" },
+                new[] { "vehicle", "transport" },
+                new[] { "happy", "joyful" },
+                new[] { "joyful", "glad" },
+                new[] { "sad", "unhappy" }
+            };
+
+            // act
+            var result = new Strings.Synonyms().AreSynonyms(synonymGroups);
+
+            // assert
+            var expected = new string[][]
+            {
+                ["car", "automobile", "vehicle", "transport"],
+                ["happy", "joyful", "glad"],
+                ["sad", "unhappy"]
+            };
+
+            Assert.That(result, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void GivenList_WhenNoSynonymsExists_ThenReturnOriginalGroups()
+        {
+            // arrange
+            var synonymGroups = new List<string[]>
+            {
+                new[] { "car", "automobile" },
+                new[] { "happy", "joyful" },
+                new[] { "sad", "unhappy " }
+            };
+
+            // act
+            var result = new Strings.Synonyms().AreSynonyms(synonymGroups);
+
+            // assert
+            Assert.That(result, Is.EquivalentTo(synonymGroups));
+        }
+    }
+}
